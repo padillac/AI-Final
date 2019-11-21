11663b53a5954a79a5d0b53eb0fc6cb99e2f992b
padillac <shinimaninima@gmail.com>
2019-11-21 13:51:05 -0600

    upgraded HiddenPrints to stifle stderr as well as stdout

--- helpers/HiddenPrints.py
+++ helpers/HiddenPrints.py
@@ -1,4 +1,5 @@
-# From Stack Overflow, stifles some annoying built-in prints in other libraries
+# Original code from Stack Overflow, modified by Chris Padilla.
+# Stifles some annoying built-in prints in other libraries
 # https://stackoverflow.com/questions/8391411/suppress-calls-to-print-python
 
 import os, sys
@@ -6,8 +7,12 @@ import os, sys
 class HiddenPrints:
     def __enter__(self):
         self._original_stdout = sys.stdout
+        self._original_stderr = sys.stderr
         sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
+        sys.stderr.close()
         sys.stdout = self._original_stdout
+        sys.stderr = self._original_stderr
