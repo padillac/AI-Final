f015be321f837d61fd7271ceda9015c8691e4c57
padillac <shinimaninima@gmail.com>
2019-11-21 12:48:58 -0600

    changed folder name to classes

--- recommender.py
+++ recommender.py
@@ -3,7 +3,7 @@ import time
 
 #Allow local files to be imported
 sys.path.insert(1, 'helpers/')
-sys.path.insert(1, 'functions/')
+sys.path.insert(1, 'classes/')
 
 from SongLoader import SongLoader #code that generates list of all tracks
 from tqdm import tqdm #progress bars
