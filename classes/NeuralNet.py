8247735a717ec595c0805f3c14cb4b3262933796
padillac <shinimaninima@gmail.com>
2019-11-21 16:41:47 -0600

    added softmax activation so neuralnet can actually classify examples

--- classes/NeuralNet.py
+++ classes/NeuralNet.py
@@ -14,3 +14,15 @@ class NeuralNet:
 
     def buildModel(self):
         self.model = Sequential()
+        self.model.add(Dense(13, input_shape=(13,), activation='relu'))
+        self.model.add(Dense(1, activation='softmax')) # Output layer
+
+        self.model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+
+
+    def trainModel(self, x_train, y_train):
+        self.model.fit(x=x_train, y=y_train, batch_size=5, epochs=20, verbose=2, validation_split=0.1, shuffle=False)
+
+
+    def predictPreferences(self, x_data):
+        pass
