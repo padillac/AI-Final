fd9d39ec042ad14e2f06d27619c6125d6d1903ec
padillac <shinimaninima@gmail.com>
2019-11-22 17:03:00 -0600

    added neural net utility functions and worked on model

--- classes/NeuralNet.py
+++ classes/NeuralNet.py
@@ -1,7 +1,10 @@
-from HiddenPrints import HiddenPrints #stifle built-in print statements
-with HiddenPrints():
+import numpy as np
+from HiddenPrints import HiddenErrors #stifle built-in print statements
+with HiddenErrors():
     from keras.models import Sequential
-    from keras.layers import Dense, Activation
+    from keras.layers import Dense, Activation, Dropout
+    from keras.optimizers import RMSprop
+    from keras.utils import plot_model
 
 
 
@@ -15,14 +18,37 @@ class NeuralNet:
     def buildModel(self):
         self.model = Sequential()
         self.model.add(Dense(13, input_shape=(13,), activation='relu'))
-        self.model.add(Dense(1, activation='softmax')) # Output layer
+        self.model.add(Dense(100, activation='relu'))
+        self.model.add(Dropout(0.2))
+        self.model.add(Dense(100, activation='relu'))
+        self.model.add(Dense(2, activation='softmax')) # Output layer
 
-        self.model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+        #opt = RMSprop(learning_rate=0.0001, decay=1e-6)
+        self.model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+    def displayModel(self):
+        self.model.summary()
+
+    def plotModel(self, path='model.png'):
+        plot_model(self.model, show_shapes=True, to_file=path)
+
 
 
     def trainModel(self, x_train, y_train):
-        self.model.fit(x=x_train, y=y_train, batch_size=5, epochs=20, verbose=2, validation_split=0.1, shuffle=False)
+        with HiddenErrors():
+            self.model.fit(x=x_train, y=y_train, batch_size=32, epochs=25, verbose=2, validation_split=0.1, shuffle=True)
 
 
     def predictPreferences(self, x_data):
-        pass
+        return self.model.predict(x_data)
+
+
+
+
+
+
+
+
+
+#
