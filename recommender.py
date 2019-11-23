a3b0d57a2202e72993cde8d75a2eb0bee6b1fa93
padillac <shinimaninima@gmail.com>
2019-11-22 21:50:09 -0600

    working on recommender command line interface

--- recommender.py
+++ recommender.py
@@ -4,6 +4,8 @@ import time
 import math
 from tqdm import tqdm #progress bars
 import numpy as np
+#from flask import Flask #allow webapp capabilities
+
 
 #Allow local files to be imported
 sys.path.insert(1, 'helpers/')
@@ -12,44 +14,59 @@ sys.path.insert(1, 'classes/')
 from SongLoader import SongLoader #code that generates list of all tracks
 from NeuralNet import NeuralNet #object that generates recommendations
 from DataManager import DataManager #object that manages Spotify and NeuralNet data
+#import FlaskFunctions as ff #functions that generate flask pages
+
+
 
 
 
 
 def getBestPredictedSong(nn, dm):
-    unknownSongData = dm.getUnknownSongData()
+    unknownSongData, indexTranslator = dm.getUnknownSongData()
     print("unknownSongData")
-    print(unknownSongData)
+    print(unknownSongData.shape)
     predictions = nn.predictPreferences(unknownSongData)
     print("PREDICTIONS:-----")
-    print(predictions)
+    print(predictions.shape)
     print("max prediction:")
-    print(predictions.indexOf(max(predictions)))
+    maxIndex = np.argmax(predictions, axis=0)[0]
+    print("index:", maxIndex)
+    print(predictions[maxIndex])
+    print(dm.getTrackData(indexTranslator[maxIndex])['name'])
+
+
+
+
+def initializeDataManager():
+    dm = DataManager()
+
+    t1 = time.time()
+    loader = SongLoader()
+    dm.loadTrackData(loader.loadSongs())
+    t2 = time.time()
+    print("---- {} tracks loaded. {} hours, {} minutes and {} seconds".format(len(dm.trackData), math.floor((t2-t1)/3600), math.floor((t2-t1)/60), round((t2-t1)%60, 4)))
+
+    return dm
+
+
+
+
 
 
 
 
 def main():
     #Initialize DataManager
-    dm = DataManager()
+    dm = initializeDataManager()
 
 
-    t1 = time.time()
-    loader = SongLoader()
-    dm.loadTrackData(loader.loadSongs())
-    t2 = time.time()
-    print("----Tracks loaded. {} hours, {} minutes and {} seconds".format(math.floor((t2-t1)/3600), math.floor((t2-t1)/60), round((t2-t1)%60, 4)))
-
-
-
-
-
+    #Load/Create preference data
     if os.path.isfile('preference-data-cache'):
         dm.loadPreferencesFromFile('preference-data-cache')
     else:
         print("No training data found. Creating some known training data")
 
-        for t in dm.getTrackIterator():
+        for id, t in dm.getTrackIterator():
             print("{}, {} -- Listen: {}".format(t['name'], t['artists'][0]['name'], t['external_urls']['spotify']))
             designation = input("Like? 1 or 0 (q to stop deciding): ")
             if designation == '':
@@ -60,11 +77,13 @@ def main():
                 print("liked")
             if int(designation) == 0:
                 print("disliked")
-            dm.updateKnownData(t['id'], int(designation))
+            dm.updateKnownData(id, int(designation))
 
         print("Done creating training data")
         dm.savePreferencesToFile('preference-data-cache')
 
+    print("Generating 2,000 random preferences")
+    dm.generateRandomPreferences(2000)
 
 
     print("----------TRAINING NEURAL NET-----------")
@@ -78,16 +97,11 @@ def main():
 
     print("-------------NEURAL NET RECOMMENDATIONS-----------")
 
-
-    #for t in dm.getTrackIterator():
+    #for id, t in dm.getTrackIterator():
     #    print("{}, {} --".format(t['name'], t['artists'][0]['name']))
-    #    nn.predictPreferences(dm.getTrackNeuralNetArray(t['id']))
+    #    nn.predictPreferences(dm.getTrackNeuralNetArray(id))
 
     getBestPredictedSong(nn, dm)
-
-
-
-
 
 
 
