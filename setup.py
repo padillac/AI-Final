1c7e7eb8dfa8e79f73c5e48de5de4a44977789d9
padillac <shinimaninima@gmail.com>
2019-11-23 19:46:00 -0600

    tweaking setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+#This script will automatically use Pip to install any dependencies that you don't already have installed
+#It will install new packages to your user directory only to avoid permissions issues
 import subprocess
 import sys
+
+
+
 
 def install(package):
     subprocess.call([sys.executable, "-m", "pip", "install", "--user", package])
 
 
 install('spotipy')
+install('tqdm')
+install('graphviz')
+install('pydot')
+install('keras')
